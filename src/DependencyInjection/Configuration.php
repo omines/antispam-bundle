e0f95ba7f1038a9f8a5253b5bb887a6be2070e2a
Niels Keurentjes <niels.keurentjes@omines.com>
2023-11-07 09:51:10 +0100

    Modularize bundle configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -14,6 +14,7 @@ namespace Omines\AntiSpamBundle\DependencyInjection;
 
 use Omines\AntiSpamBundle\Form\Type\SubmitTimerType;
 use Omines\AntiSpamBundle\Type\Script;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
@@ -25,7 +26,7 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder('antispam');
         $rootNode = $treeBuilder->getRootNode();
 
-        $rootNode
+        $children = $rootNode
             ->children()
                 ->booleanNode('passive')
                     ->info('Global default for whether included components should cause hard failures')
@@ -34,95 +35,157 @@ class Configuration implements ConfigurationInterface
                 ->booleanNode('stealth')
                     ->info('Global default for whether included components issue verbose or stealthy error messages')
                     ->defaultFalse()
+                ->end();
+
+        $this->addProfileSection($children);
+
+        $children->end();
+
+        return $treeBuilder;
+    }
+
+    private function addProfileSection(NodeBuilder $rootNode): void
+    {
+        $profile = $rootNode
+            ->arrayNode('profiles')
+                ->info('A named list of different profiles used throughout your application')
+                ->useAttributeAsKey('name')
+                ->arrayPrototype()
+                    ->info('Name the profile')
+                    ->validate()->always(function (array $profile) {
+                        if (empty($profile['banned_phrases'])) {
+                            unset($profile['banned_phrases']);
+                        }
+
+                        return $profile;
+                    })->end()
+                ->children()
+                    ->booleanNode('stealth')
+                        ->info('Defines whether measures in this profile issue stealthy error messages')
+                        ->defaultTrue()
+                    ->end()
+                    ->booleanNode('passive')
+                        ->info('Passive mode will not make any of the included checks actually fail validation, they will still be logged')
+                        ->defaultFalse()
+                    ->end()
+        ;
+
+        $this->addBannedMarkupSection($profile);
+        $this->addBannedPhrasesSection($profile);
+        $this->addBannedScriptsSection($profile);
+        $this->addHoneypotSection($profile);
+        $this->addTimerSection($profile);
+        $this->addUrlCountSection($profile);
+    }
+
+    private function addBannedMarkupSection(NodeBuilder $profile): void
+    {
+        $profile
+            ->arrayNode('banned_markup')
+                ->info('Defines whether to disallow content resembling markup languages like HTML and BBCode')
+                ->children()
+                    ->booleanNode('html')->defaultTrue()->end()
+                    ->booleanNode('bbcode')->defaultTrue()->end()
                 ->end()
-                ->arrayNode('profiles')
-                    ->info('A named list of different profiles used throughout your application')
-                    ->useAttributeAsKey('name')
-                    ->arrayPrototype()
-                        ->info('Name the profile')
-                        ->validate()->always(function (array $profile) {
-                            if (empty($profile['banned_phrases'])) {
-                                unset($profile['banned_phrases']);
-                            }
+            ->end()
+        ;
+    }
 
-                            return $profile;
-                        })->end()
-                        ->children()
-                            ->booleanNode('stealth')
-                                ->info('Defines whether measures in this profile issue stealthy error messages')
-                                ->defaultTrue()
-                            ->end()
-                            ->booleanNode('passive')
-                                ->info('Passive mode will not make any of the included checks actually fail validation, they will still be logged')
-                                ->defaultFalse()
-                            ->end()
-                            ->arrayNode('banned_markup')
-                                ->info('Defines whether to disallow content resembling markup languages like HTML and BBCode')
-                                ->children()
-                                    ->booleanNode('html')->defaultTrue()->end()
-                                    ->booleanNode('bbcode')->defaultTrue()->end()
-                                ->end()
-                            ->end()
-                            ->arrayNode('banned_phrases')
-                                ->info('Simple array of phrases which are rejected when encountered in a submitted text field')
-                                ->defaultValue([])
-                                ->scalarPrototype()->end()
-                            ->end()
-                            ->arrayNode('banned_scripts')
-                                ->info('Banned script types, like Cyrillic or Arabic (see docs for commonly used ISO 15924 names)')
-                                ->beforeNormalization()
-                                    ->always(fn (array|string $v) => isset($v[0]) ? ['scripts' => is_string($v) ? [$v] : $v] : $v)
-                                ->end()
-                                ->children()
-                                    ->arrayNode('scripts')
-                                        ->requiresAtLeastOneElement()
-                                        ->scalarPrototype()
-                                            ->validate()->always(function (string $v) {
-                                                if (null === Script::tryFrom($v = mb_strtolower($v))) {
-                                                    throw new InvalidConfigurationException(sprintf('"%s" is not a valid ISO-15924 script name, look in class %s for all valid options', $v, Script::class));
-                                                }
+    private function addBannedPhrasesSection(NodeBuilder $profile): void
+    {
+        $profile
+            ->arrayNode('banned_phrases')
+                ->info('Simple array of phrases which are rejected when encountered in a submitted text field')
+                ->defaultValue([])
+                ->scalarPrototype()->end()
+            ->end()
+        ;
+    }
 
-                                                return $v;
-                                            })->end()
-                                        ->end()
-                                    ->end()
-                                    ->integerNode('max_characters')->min(0)->defaultNull()->end()
-                                    ->floatNode('max_percentage')->min(0)->max(100)->defaultValue(0)->end()
-                                ->end()
-                            ->end()
-                            ->arrayNode('honeypot')
-                                ->info('Inject an invisible honeypot field in forms, baiting spambots to fill it in')
-                                ->beforeNormalization()
-                                    ->ifString()
-                                    ->then(fn (string $v) => ['field' => $v])
-                                ->end()
-                                ->children()
-                                    ->scalarNode('field')
-                                        ->info('Base name of the injected field')
-                                        ->isRequired()
-                                    ->end()
-                                ->end()
-                            ->end()
-                            ->scalarNode('max_urls')
-                                ->info('Maximum number of URLs permitted in text fields')
-                            ->end()
-                            ->arrayNode('timer')
-                                ->info('Verify that time between retrieval and submission of a form is within human boundaries')
-                                ->children()
-                                    ->scalarNode('field')
-                                        ->info('Base name of the injected field')
-                                        ->defaultValue('__antispam_time')
-                                    ->end()
-                                    ->integerNode('min')->defaultValue(SubmitTimerType::DEFAULT_MIN)->min(0)->end()
-                                    ->integerNode('max')->defaultValue(SubmitTimerType::DEFAULT_MAX)->min(60)->end()
-                                ->end()
-                            ->end()
+    private function addBannedScriptsSection(NodeBuilder $profile): void
+    {
+        $profile
+            ->arrayNode('banned_scripts')
+                ->info('Banned script types, like Cyrillic or Arabic (see docs for commonly used ISO 15924 names)')
+                ->beforeNormalization()
+                    ->always(fn (array|string $v) => isset($v[0]) ? ['scripts' => is_string($v) ? [$v] : $v] : $v)
+                ->end()
+                ->children()
+                    ->arrayNode('scripts')
+                        ->requiresAtLeastOneElement()
+                        ->scalarPrototype()
+                            ->validate()->always(function (string $v) {
+                                if (null === Script::tryFrom($v = mb_strtolower($v))) {
+                                    throw new InvalidConfigurationException(sprintf('"%s" is not a valid ISO-15924 script name, look in class %s for all valid options', $v, Script::class));
+                                }
+
+                                return $v;
+                            })->end()
+                        ->end()
+                    ->end()
+                    ->integerNode('max_characters')->min(0)->defaultNull()->end()
+                    ->floatNode('max_percentage')->min(0)->max(100)->defaultValue(0)->end()
+                ->end()
+            ->end()
+        ;
+    }
+
+    private function addHoneypotSection(NodeBuilder $profile): void
+    {
+        $profile
+            ->arrayNode('honeypot')
+                ->info('Inject an invisible honeypot field in forms, baiting spambots to fill it in')
+                    ->beforeNormalization()
+                        ->ifString()
+                        ->then(fn (string $v) => ['field' => $v])
+                    ->end()
+                    ->children()
+                        ->scalarNode('field')
+                            ->info('Base name of the injected field')
+                            ->isRequired()
                         ->end()
                     ->end()
                 ->end()
             ->end()
         ;
+    }
 
-        return $treeBuilder;
+    private function addTimerSection(NodeBuilder $profile): void
+    {
+        $profile
+            ->arrayNode('timer')
+                ->info('Verify that time between retrieval and submission of a form is within human boundaries')
+                ->children()
+                    ->scalarNode('field')
+                        ->info('Base name of the injected field')
+                        ->defaultValue('__antispam_time')
+                    ->end()
+                    ->integerNode('min')->defaultValue(SubmitTimerType::DEFAULT_MIN)->min(0)->end()
+                    ->integerNode('max')->defaultValue(SubmitTimerType::DEFAULT_MAX)->min(60)->end()
+                ->end()
+            ->end()
+        ;
+    }
+
+    private function addUrlCountSection(NodeBuilder $profile): void
+    {
+        $profile
+            ->arrayNode('url_count')
+                ->info('Configure limits to number of URLs permitted in text fields')
+                ->beforeNormalization()
+                    ->always(fn (mixed $v) => is_int($v) ? ['max' => $v] : $v)
+                ->end()
+                ->children()
+                    ->integerNode('max')
+                        ->info('Maximum number of URLs accepted per text field')
+                        ->defaultNull()->min(0)
+                    ->end()
+                    ->integerNode('max_identical')
+                        ->info('Maximum number of identical URLs accepted per text field')
+                        ->defaultNull()->min(0)
+                    ->end()
+                ->end()
+            ->end()
+        ;
     }
 }
