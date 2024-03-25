4cbe0045bf9ef26260f86878e0f9571758ad57a1
Niels Keurentjes <niels.keurentjes@omines.com>
2024-03-25 12:58:03 +0100

    PHPunit 11 and Infection 0.28

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,7 +11,13 @@
 declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\ErrorHandler;
 
 require dirname(__DIR__) . '/vendor/autoload_runtime.php';
+
+/*
+ * Temporary workaround as per https://github.com/symfony/symfony/issues/53812#issuecomment-1962740145
+ */
+set_exception_handler([new ErrorHandler(), 'handleException']);
 
 (new Dotenv())->loadEnv(__DIR__ . '/Fixture/.env');
