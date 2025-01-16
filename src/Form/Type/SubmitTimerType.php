384c0336d7c7bcb90d1d98149c5123844f6496dd
Niels Keurentjes <niels.keurentjes@omines.com>
2025-01-16 11:11:15 +0100

    Extend PHPStan ignore config to support multiple PHP versions

--- src/Form/Type/SubmitTimerType.php
+++ src/Form/Type/SubmitTimerType.php
@@ -98,8 +98,6 @@ class SubmitTimerType extends NonInteractiveAntiSpamType
         $ip = $this->requestStack->getMainRequest()?->getClientIp() ?? self::NO_IP;
         $ts = $this->getTimestampMilliseconds();
         $secret = $options['secret'];
-
-        assert(is_array($view->vars));
         $view->vars['value'] = \base64_encode(implode('|', [$ip, $ts, hash('sha256', "$ts|$ip|$secret")]));
     }
 
